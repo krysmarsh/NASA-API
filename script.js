fb5fd4a2649b449e8f20de2702155e13293b6c63
Krystle Marshall <krystlemarshall@Krystles-Air.hsd1.in.comcast.net>
2021-06-26 17:59:52 -0400

    NASA API

---
+++ script.js
@@ -0,0 +1,45 @@
+const baseURL = 'https://api.nasa.gov/planetary/apod';
+const key = 'GYgwraiCPMZGV5g4bbvJ9Kqjpy6GNZX5ov8TSuW6';
+
+const card = document.querySelector('.card');
+const cardBody = document.querySelector('.card-body');
+const moreInfoBtn = document.querySelector('.btn');
+const modalBody = document.querySelector('.modal-body');
+const modalTitle = document.querySelector('.modal-title');
+
+fetch(`${baseURL}?api_key=${key}`)
+    .then(response => response.json())
+    .then(json => displayImage(json));
+
+function displayImage(spaceObject) {
+    console.log(spaceObject);
+
+
+let img = document.createElement('img');
+img.className = 'card-img-top';
+img.src = spaceObject.hdurl;
+
+let title = document.createElement('h1');
+title.className = 'card-title';
+title.innerText = spaceObject.title;
+title.style = 'font-family: space-age;';
+
+let date = document.createElement('p');
+date.className = 'card-text';
+date.innerText = spaceObject.date;
+date.style.fontFamily = 'kiona-regular';
+
+let expl = document.createElement('p');
+expl.innerText = spaceObject.explanation;
+expl.style.fontFamily = 'kiona-regular';
+
+modalTitle.innerText = spaceObject.title;
+modalTitle.style.fontFamily = 'space-age';
+
+card.insertBefore(img, cardBody);
+cardBody.insertBefore(date, moreInfoBtn);
+cardBody.insertBefore(title, moreInfoBtn);
+modalBody.appendChild(expl);
+    
+
+}
